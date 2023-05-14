cf4a088bfe95bcee5492b636724a6ba3ffe7ee74
TIISETSO\TiisetsoM <tiisetso88@live.com>
2023-05-14 15:21:45 +0200

    Created skeleton of project

---
+++ API/Entities/User.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Entities
+{
+    public class User
+    {
+        
+        public int Id {get;set;}
+
+        public string UserName {get;set;}    
+    }
+}
